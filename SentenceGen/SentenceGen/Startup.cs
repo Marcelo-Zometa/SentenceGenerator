ef446e0e2d803eed1652c2e2f4e981a1a77f93d2
Marcelo Paniagua <marcelo.zometa@hotmail.com>
2019-11-12 01:14:11 -0700

    Just erased my try on connecting to the database

--- SentenceGen/SentenceGen/Startup.cs
+++ SentenceGen/SentenceGen/Startup.cs
@@ -31,7 +31,7 @@ namespace SentenceGen
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
